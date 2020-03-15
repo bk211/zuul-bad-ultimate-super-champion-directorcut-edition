cf54ed3470f76bdf435c3fd76a4a52f392165d67
playmaker <chaoleicai@gmail.com>
2020-03-15 15:20:55 +0100

    fixed test

--- Test.java
+++ Test.java
@@ -2,8 +2,7 @@ public class Test{
 
     public static void main(String[] args) {
         RoomFileReader foo = new RoomFileReader("RoomData.csv");
-        //System.out.println(foo.records);
         System.out.println(foo.ParseRooms());
-        
+        System.out.println(foo.ParseRooms().get("cuisine").getLongDescription());
     }
 }
