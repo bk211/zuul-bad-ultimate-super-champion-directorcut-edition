e69b909720392d59bbdf77c3139eb658350e9485
playmaker <chaoleicai@gmail.com>
2020-03-15 15:24:20 +0100

    bring new way to edit rooms data

--- GameModel.java
+++ GameModel.java
@@ -13,7 +13,7 @@ public class GameModel extends Observable
     
     private Room currentRoom;
     private Parser parser;
-    private HashMap<String,Room> rooms = new HashMap<String, Room>();
+    private HashMap<String,Room> rooms;
     private Stack<Room> pastRooms;
     private GameView gameView;
 
@@ -41,59 +41,9 @@ public class GameModel extends Observable
      */
     private void createRooms()
     {
-        Room parking = new Room("in the parking");        
-        Room aile_gauche = new Room("in the left wing");
-        Room cargo1 = new Room("in the cargo1");
-        Room cargo2 = new Room("in the cargo2");
-        Room dock1 = new Room("in the dock1");
-        Room dock2 = new Room("in the dock2");
-        Room reactor = new Room("in the reactor room");
-        Room rest = new Room("in the rest room");
-        Room vestiaire = new Room("in the change room");
-        Room aile_droite = new Room("in the right wing");
-        Room cuisine = new Room("in the kitchen");
-        Room escalier = new Room("in the staires");
-        Room hall = new Room("in the Hall");
-        Room commandement = new Room("in the head quarter");
-        
-        parking.setImageLink("img/castle.gif");
-        aile_gauche.setImageLink("img/courtyard.gif");
-        rooms.put("parking", parking);
-        
-        parking.setExit("east", aile_gauche);
-        cargo2.setExit("cargo1", cargo1);
-        cargo1.setExit("north", cargo2);
-        cargo1.setExit("south", aile_gauche);
-        cargo1.setExit("east", rest);
-        aile_gauche.setExit("north", cargo1);
-        aile_gauche.setExit("south", dock1);
-        aile_gauche.setExit("up", hall);
-        dock1.setExit("north", aile_gauche);
-        reactor.setExit("sud", rest);
-        rest.setExit("north", reactor);
-        rest.setExit("west", cargo1);
-        rest.setExit("south", hall);
-        rest.setExit("east", vestiaire);
-        hall.setExit("north", rest);
-        hall.setExit("west", aile_gauche);
-        hall.setExit("south", commandement);
-        hall.setExit("east", aile_droite);
-        commandement.setExit("north", hall);
-        vestiaire.setExit("west", rest);
-        vestiaire.setExit("south", aile_droite);
-        aile_droite.setExit("north", vestiaire);
-        aile_droite.setExit("west", hall);
-        aile_droite.setExit("south", cuisine);
-        aile_droite.setExit("east", dock2);
-        dock2.setExit("north", aile_droite);
-        cuisine.setExit("west", aile_droite);
-        cuisine.setExit("south", escalier);
-        escalier.setExit("north", cuisine);
-        
-        
-        //setExits(Room north, Room east, Room south, Room west) 
-        // initialise room exits
-        currentRoom = parking;  // start game outside
+        rooms = new RoomFileReader("RoomData.csv").ParseRooms();
+
+        currentRoom = rooms.get("parking");  // start game outside
     }
 
     /**
