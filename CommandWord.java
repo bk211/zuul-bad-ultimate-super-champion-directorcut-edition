d5c98c0e5bfa22d6d434ed763e8ae232286c451e
playmaker <chaoleicai@gmail.com>
2020-03-17 23:26:37 +0100

    better enums

--- CommandWord.java
+++ CommandWord.java
@@ -1,4 +1,26 @@
-public enum CommandWord
-{
-    GO,QUIT,HELP,LOOK,EAT,BACK,TEST,TAKE,DROP,MINE,UNKNOWN;
+/*** Representations for all the valid command words for the game* along with a string in a particular language.* 
+ * @author Michael Kolling and David J. Barnes
+ * @version 2006.03.30
+*/
+
+public enum CommandWord{    
+    // A value for each command word along with its
+    // corresponding user interface string.
+    GO("go"), QUIT("quit"), HELP("help"), LOOK("look"), EAT("eat"), BACK("back"),TEST("test"),TAKE("take"),DROP("drop"),MINE("mine"), UNKNOWN("?");
+
+    // The command string.
+    private String commandString;
+    /*** 
+     * Initialize with the corresponding command word.
+     * @param commandString The command string.
+    */
+    CommandWord(String commandString){
+        this.commandString = commandString;
+    }
+    /*** 
+     * @return The command word as a string.
+    */
+    public String toString(){
+        return commandString;
+    }
 }
