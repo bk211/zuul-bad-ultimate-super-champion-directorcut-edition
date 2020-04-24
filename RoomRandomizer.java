3cb0544b37015f767e771e9babc607d36ba0523e
Amarouche Lies <55063248+lies98@users.noreply.github.com>
2020-04-24 19:50:00 +0200

    Add files via upload

---
+++ RoomRandomizer.java
@@ -0,0 +1,21 @@
+
+import java.util.HashMap;
+import java.util.Random;
+public class RoomRandomizer
+{
+    private HashMap <String, Room> myRooms;
+    private Random randomize;
+
+    public RoomRandomizer (final HashMap <String, Room> myRooms)
+    {
+        this.myRooms = myRooms;
+        this.randomize = new Random();
+    }
+
+    public Room randomizeRooms()
+
+    {
+        Room[] roomTab = myRooms.values().toArray (new Room[0]);
+        return roomTab[randomize.nextInt (myRooms.size())];
+    }
+}
