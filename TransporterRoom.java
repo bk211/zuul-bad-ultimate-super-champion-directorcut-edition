c26bc5536d2f8a607ec2839afc750adac0758af0
Amarouche Lies <55063248+lies98@users.noreply.github.com>
2020-04-24 19:45:32 +0200

    Add files via upload

---
+++ TransporterRoom.java
@@ -0,0 +1,23 @@
+
+import java.util.HashMap;
+import java.util.Random;
+public class TransporterRoom extends Room
+{
+    private  RoomRandomizer randomRoom;
+    private HashMap <String, Room> myRooms;
+
+    public TransporterRoom (final String description, final HashMap <String, Room> Rooms)
+    {
+        super(description);
+        this.myRooms = Rooms;
+
+    }
+
+    @Override
+    public Room getExit (final String direction)
+    {
+        randomRoom = new RoomRandomizer(myRooms);
+        return randomRoom.randomizeRooms();
+
+    }
+}
